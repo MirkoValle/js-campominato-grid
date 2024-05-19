931a7c763ed5e25922ce41b2cc645a35bd895b52
MirkoValle <mirko.valle20@gmail.com>
2024-05-19 20:20:55 +0200

    Added working if for choosing grid number based on the level mode

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 const btn = document.querySelector("button");
 const gridEl = document.querySelector("section");
-const modeEl = document.getElementById("mode");
-const mode = modeEl.value;
-
-
-
 
 btn.addEventListener("click", function () {
+    let modeEl = document.getElementById("mode");
+    let mode = modeEl.value;
+    let cellN;
+
+    if (mode === "easy") {
+        cellN = 49;
+    } else if (mode === "medium") {
+        cellN = 81;
+    } else {
+        cellN = 100;
+    }
 
     gridEl.innerHTML = ""
-
-    for (let index = 0; index < 100; index++) {
-
+    for (let index = 0; index < cellN; index++) {
         const cellEl = document.createElement("article");
-        cellEl.classList.add("cell");
+        cellEl.classList.add("cell", mode);
         cellEl.append(index + 1);
         cellEl.addEventListener("click", function () {
             cellEl.classList.toggle("active")
@@ -21,11 +25,5 @@ btn.addEventListener("click", function () {
         });
 
         gridEl.appendChild(cellEl);
-
-
     }
-
-
-
-
 })
